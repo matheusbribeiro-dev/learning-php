67c3de332fabd624647b54c51f85527c59663f24
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-09-07 00:07:41 -0300

    Class 16: Create a new files: exercice.css and .php

---
+++ exercice.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!--Title-->
+    <title>Exercício</title>
+
+    <!--Meta tags-->
+    <meta charset="UTF-8" />
+
+    <!--Links-->
+    <link
+    href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+    rel="stylesheet"
+    />
+    <link rel="stylesheet" href="assets/css/style.css" />
+  </head>
+  <body>
+    <header class="header">
+      <h1>Curso PHP</h1>
+      <h2>Visualização do exercício</h2>
+    </header>
+    <main class="main">
+      <!--div content-->
+      <div class="content">
+
+      </div>
+      <!--//div content-->
+    </main>
+    <footer class="footer">COD3R & ALUNOS © <?= date('Y'); ?> </footer>
+  </body>
+</html>
