8bcc404df3b8303e470947aeb146b364329c5f63
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-09-07 20:10:41 -0300

    Class 17 - minute 1:x: Added two links('Sem formatacao' and 'voltar') in exercice.php

--- exercice.php
+++ exercice.php
@@ -1,31 +1,37 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <!--Title-->
-    <title>Exercício</title>
+	<head>
+		<!--Title-->
+		<title>Exercício</title>
 
-    <!--Meta tags-->
-    <meta charset="UTF-8" />
+		<!--Meta tags-->
+		<meta charset="UTF-8" />
 
-    <!--Links-->
-    <link
-    href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
-    rel="stylesheet"
-    />
-    <link rel="stylesheet" href="assets/css/style.css" />
-  </head>
-  <body>
-    <header class="header">
-      <h1>Curso PHP</h1>
-      <h2>Visualização do exercício</h2>
-    </header>
-    <main class="main">
-      <!--div content-->
-      <div class="content">
+		<!--Links-->
+		<link
+		href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap"
+		rel="stylesheet"
+		/>
+		<link rel="stylesheet" href="assets/css/style.css" />
+	</head>
+	<body>
+		<header class="header">
+			<h1>Curso PHP</h1>
+			<h2>Visualização do exercício</h2>
+		</header>
+		<!--nav-->
+		<nav class="navigation">
+			<a href="#" class="green">Sem formatação</a>
+			<a href="/index.php" class="red">Voltar</a>
+		</nav>	
+		<!--//nav-->
+		<main class="main">
+			<!--div content-->
+			<div class="content">
 
-      </div>
-      <!--//div content-->
-    </main>
-    <footer class="footer">COD3R & ALUNOS © <?= date('Y'); ?> </footer>
-  </body>
+			</div>
+			<!--//div content-->
+		</main>
+		<footer class="footer">COD3R & ALUNOS © <?= date('Y'); ?> </footer>
+	</body>
 </html>
