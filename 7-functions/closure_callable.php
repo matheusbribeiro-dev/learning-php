551eba7a082b284ea244dbad2ec5a2b0520cebc3
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-10-16 15:28:56 -0300

    Closure and Callable completed

--- 7-functions/closure_callable.php
+++ 7-functions/closure_callable.php
@@ -1,8 +1,15 @@
 <div class="title">Closure e Callable</div>
 
 <?php
+    /*Ao definir uma função anônima internamente o php insere está função numa classe*/
     $som1 = function ($a, $b) {
         return $a + $b;
     };
 
-    echo $som1(2, 3);
+    echo $som1(2, 3) . '<br>';
+    echo (is_callable($som1) ? 'Sim': 'Não') . '<br>';
+
+    $som1 = 1;
+    echo (is_callable($som1) ? 'Sim': 'Não') . '<br>';
+
+    var_dump($som1);
