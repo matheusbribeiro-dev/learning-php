22688074ad57bd34405f2a1f1e3c96a0eeb2c106
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-10-20 14:24:54 -0300

    Class 137: class in code

--- 8-class_objects/class.php
+++ 8-class_objects/class.php
@@ -1,4 +1,30 @@
 <div class="title">Primeira classe</div>
 
 <?php
-    
+    class Client {
+        //atributos
+        public $name = 'Anônimo';/*O 'public' serve para dizer que este atributo poderá
+        ser 'enxergado' fora da classe. Ou seja, este valor antes do nome do atributo
+        serve para definir seu nível de visibilidade e a definição do nível é
+        obrigatória*/
+        public $age = 18;
+
+        public function niceToMeet() {
+            echo "Nome: {$this->name} Idade: {$this->age}<br>";/*O 'this' serve para
+            conseguirmos acessar a variável definida no construtor da própria classe*/
+        }
+    }
+
+    $c1 = new Client();/*Palavra reservada 'new', serve para instânciar um objeto.
+    Ou seja, 'c1' nada mais é que um objeto instância da classe Client*/
+    $c1->name = 'Matheus';
+    $c1->age = 20;
+
+    $c1->niceToMeet();
+
+    /*Mais instâncias */
+    $c2 = new Client;
+    $c2->name = 'Gabriel';
+    $c2->age = 32;
+
+    $c2->niceToMeet();
