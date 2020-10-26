4cc1f1daa713935f122f41fd9d0a76f849a48f52
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-10-26 11:55:42 -0300

    Testing workflow

--- 10-namespace/basic.php
+++ 10-namespace/basic.php
@@ -1,1 +1,14 @@
+<?php namespace context;
+
+/*namespace precisa ser a primeira sentça de código */
+?>
+
 <div class="title">Exemplo básico</div>
+
+<?php
+echo __NAMESPACE__ . '<br>';
+
+const constant1 = 123;
+
+/*Definindo constante com define em namespace */
+define('context\constant2', 234);
