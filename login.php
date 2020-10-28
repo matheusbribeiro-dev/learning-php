127f110698385d9000c20b5cbf4004580e35bfad
Matheus Ribeiro <MatheusBR2019@hotmail.com>
2020-10-28 10:10:39 -0300

    class 195: front and back end from file login

--- login.php
+++ login.php
@@ -1,3 +1,44 @@
+<?php
+session_start();
+
+$isSetEmail = isset($_POST['email']);
+$isSetPassword = isset($_POST['password']);
+
+if ($isSetEmail and $isSetPassword) {
+    $users = [
+        [
+            "name" => "Aluno Cod3r",
+            "email" => "aluno@cod3r.com.br",
+            "password" => "123456",
+        ],
+        [
+            "name" => "Outro aluno",
+            "email" => "outro@email.com.br",
+            "password" => "654321",
+        ],
+    ];
+
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    foreach ($users as $user) {
+        $validEmail = $email === $user['email'];
+        $validPass = $password === $user['password'];
+
+        if ($validEmail and $validPass) {
+            $_SESSION['errors'] = null;
+            $_SESSION['user'] = $user['name'];
+
+            header('Location: index.php');
+        }
+    }
+
+    if (!$_SESSION['user']) {
+        $_SESSION['errors'] = ['Usuário/Senha inválida!'];
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
   <head>
@@ -13,8 +54,9 @@
       rel="stylesheet"
     />
     <link rel="stylesheet" href="assets/css/style.css" />
+    <link rel="stylesheet" href="assets/css/login.css" />
   </head>
-  <body>
+  <body class="login">
     <header class="header">
       <h1>Curso PHP</h1>
       <h2>Seja Bem Vindo</h2>
@@ -22,6 +64,31 @@
     <main class="main">
       <!--div content-->
       <div class="content">
+
+        <h3>Identifique-se</h3>
+        <!--sintaxe alternativa para if -->
+        <?php if ($_SESSION['errors']): ?>
+
+          <div class="errors">
+            <?php foreach ($_SESSION['errors'] as $error): ?>
+              <p><?=$error?></p>
+            <?php endforeach?>
+          </div>
+
+        <?php endif?>
+        <form action="#" method="post">
+          <div>
+            <label for="email">E-mail: </label>
+            <input type="email" name="email" id="email">
+          </div>
+
+          <div>
+            <label for="password">Senha: </label>
+            <input type="password" name="password" id="password">
+          </div>
+
+          <button>Entrar</button>
+        </form>
       </div>
       <!--//div content-->
     </main>
